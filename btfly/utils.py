1ed70f844d9cd3c39d00e4390275f775b4df1b0f
oinume <oinume@gmail.com>
2011-11-20 07:54:17 +0900

    add tests

---
+++ btfly/utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import logging
+
+def create_logger(debug_enabled=False):
+    level = logging.INFO
+    if debug_enabled:
+        level = logging.DEBUG
+    logging.basicConfig(
+        level = level,
+        format = '[%(levelname)s] %(message)s'
+    )
+    return logging
---
+++ tests/internal/test_conf_loader.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import os
+from nose.tools import eq_, ok_
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+from btfly.conf import ConfLoader
+from btfly.utils import create_logger
+
+log = create_logger(True)
+loader = ConfLoader()
+def test_01_load_yaml():
+    object = loader.load("""
+statuses: [ 'active', 'troubled' ]
+environments:
+  - { production: [ 'production', 'prd' ] }
+  - { development: [ 'development', 'dev' ] }
+""".strip(), 'yaml')
+
+    expected = {
+        'statuses': [ 'active', 'troubled' ],
+        'environments': [
+            { 'production': [ 'production', 'prd' ] },
+            { 'development': [ 'development', 'dev' ] },
+        ]
+    }
+    eq_(expected, object, "load (yaml)")

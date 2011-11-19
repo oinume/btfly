d660f5bd68c57535b0b29d8ad2a962dd0001dd0a
oinume <oinume@gmail.com>
2011-11-20 08:00:08 +0900

    add tests

--- tests/internal/test_conf_loader.py
+++ tests/internal/test_conf_loader.py
@@ -8,6 +8,7 @@ from btfly.utils import create_logger
 
 log = create_logger(True)
 loader = ConfLoader()
+
 def test_01_load_yaml():
     object = loader.load("""
 statuses: [ 'active', 'troubled' ]
@@ -24,3 +25,22 @@ environments:
         ]
     }
     eq_(expected, object, "load (yaml)")
+
+def test_01_load_json():
+    object = loader.load("""{
+"statuses": [ "active", "troubled" ],
+"environments": [
+    { "production": [ "production", "prd" ] },
+    { "development": [ "development", "dev" ] }
+]
+} """.strip(), 'json')
+
+    expected = {
+        'statuses': [ 'active', 'troubled' ],
+        'environments': [
+            { 'production': [ 'production', 'prd' ] },
+            { 'development': [ 'development', 'dev' ] },
+        ]
+    }
+    eq_(expected, object, "load (json)")
+
